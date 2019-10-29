544c1c7b63e954a76f43318e72e67913a3ff94bb
Student <student@Makerss-MacBook-Pro.local>
2019-10-29 13:20:23 +0000

    Used 'post' instead of 'get'

--- lib/app.rb
+++ lib/app.rb
@@ -16,6 +16,16 @@ get '/random_cat' do
   erb(:index)
 end
 
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
 get '/named-cat' do
   p params
   @name = params[:name]
